100b4871e0318ec5ab4a6d923860a88edb73605c
imjnnanna <imjnnanna@gmail.com>
2021-10-17 22:32:29 -0400

    created working version of bot

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ import utils
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix="!")
+client = discord.Client()
 
 # load potw yaml file
 potw_file = open('potw.yaml')
@@ -23,6 +24,7 @@ current_potw = potw_contents['current-potw']
 async def on_message(message):
     await bot.process_commands(message)
 
+
 @bot.command(brief="Print the current POTW")
 async def potw(ctx):
     await ctx.channel.send(current_potw['text'])
@@ -30,20 +32,45 @@ async def potw(ctx):
 
 @bot.command(brief="Submit answer to the current POTW")
 async def submit(ctx, arg):
+    submitter = ctx.author.id
     if current_potw['simple-solution'] == True:
         if arg == current_potw['solution']:
             # assign points
-            func.assign_points(bot.get_user(user_id), current_potw['points'])
+            func.assign_points(submitter, current_potw['points'])
             await ctx.channel.send("Your solution was correct; you have gained " + str(current_potw['points']) + " points!")
+        else:
+            await ctx.channel.send("Your solution was incorrect; please try again.")
     else:
         # send solution to mods
         await ctx.channel.send("Thanks! Your solution was sent to the mods for review.")
+        channel = bot.get_channel(899463961836142664) # organizer-only channel 
+        await channel.send(f"From <@{submitter}>: " + str(arg))
+
 
 @bot.command()
-@commands.has_rile('Mod')
+@commands.has_role('organizer')
 async def assign_point(ctx, user, pointvalue):
     # add point value to database
-    await ctx.channel.send(str(pointvalue) + " has been assigned to user " + user)
+    func.assign_points(user, pointvalue)
+    await ctx.channel.send(f"{pointvalue} points has been assigned to <@{user}>")
+
+@bot.command(brief="Print leaderboard")
+@commands.has_role('organizer')
+async def leaderboard(ctx):
+    with open('leaderboard.yaml', 'r') as f:
+        leaderboard = yaml.load(f, Loader = yaml.FullLoader)
+    sorted_leaderboard = sorted(leaderboard, key=lambda d: d['points'], reverse=True)
+
+    message = ""
+
+    for i in range(0, len(sorted_leaderboard)):
+        message += f"{i+1} | <@{sorted_leaderboard[i]['userID']}> with {sorted_leaderboard[i]['points']} points \n"
+    
+    await ctx.channel.send(message)
+
+#TODO: let people know how many points they have
+#TODO: find way to incorporate username without pinging people
+#TODO: problem proposals (maybe not)
 
 # run the bot
 bot.run(TOKEN)
--- func.py
+++ func.py
@@ -1,20 +1,31 @@
+import os
 import yaml
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!")
 
 def assign_points(user, points):
-	f = open('leaderboard.yaml')
-	leaderboard = yaml.load(f, Loader = yaml.FullLoader)
+    with open('leaderboard.yaml', 'r') as f:
+        leaderboard = yaml.load(f, Loader = yaml.FullLoader)
 
-	potw_file = open('potw.yaml')
-	potw_contents = yaml.load(potw_file, Loader = yaml.FullLoader)
-	current_potw = potw_contents['current-potw']
+    with open('potw.yaml', 'r') as f:
+        potw_contents = yaml.load(f, Loader = yaml.FullLoader)
+    current_potw = potw_contents['current-potw']
 
-	for username in leaderboard:
-	    if username == user:
-	    	username['points'] += points
-	    	
-	    	solved_list = username['solved-list']
-	    	solved_list.append(current_potw['potw-id'])
-	    	username['solved-list'] = solved_list
+    user_in_leaderboard = False
 
-	yaml.dump(leaderboard, f)
-	f.close()
+    for person in leaderboard:
+        if str(person['userID']) == str(user):
+            person['points'] = int(person['points']) + int(points)
+            solved_list = person['solved-list']
+            solved_list.append(current_potw['potw-id'])
+            person['solved-list'] = solved_list
+            user_in_leaderboard = True
+
+    if user_in_leaderboard == False:
+        person = {'points': int(points), 'solved-list': [current_potw['potw-id']], 'userID': int(user), 'username': bot.get_user(int(user))}
+        leaderboard.append(person)
+
+    with open('leaderboard.yaml', 'w') as f:
+        yaml.dump(leaderboard, f)
