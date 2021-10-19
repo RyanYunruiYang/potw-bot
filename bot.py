17d3f85677729cc0b7bbd15a23bab33be3460a09
imjnnanna <imjnnanna@gmail.com>
2021-10-18 22:50:50 -0400

    added final features and fixed bugs

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ import utils
 # create the bot
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
 bot = commands.Bot(command_prefix="!")
 client = discord.Client()
 
@@ -24,15 +25,20 @@ current_potw = potw_contents['current-potw']
 async def on_message(message):
     await bot.process_commands(message)
 
-
+# user commands
 @bot.command(brief="Print the current POTW")
 async def potw(ctx):
-    await ctx.channel.send(current_potw['text'])
+    await ctx.channel.send(f"Field: {current_potw['field']}\nProblem ID: {current_potw['potw-id']}\nAuthor: {current_potw['author']}\nDifficulty: {current_potw['difficulty']} jalapeños\nProblem statement: {current_potw['text']}")
 
 
 @bot.command(brief="Submit answer to the current POTW")
 async def submit(ctx, arg):
     submitter = ctx.author.id
+
+    if func.check_solved(submitter, current_potw['potw-id']) == True:
+        await ctx.channel.send("You have alreay solved this week's problem!")
+        return
+
     if current_potw['simple-solution'] == True:
         if arg == current_potw['solution']:
             # assign points
@@ -47,15 +53,35 @@ async def submit(ctx, arg):
         await channel.send(f"From <@{submitter}>: " + str(arg))
 
 
-@bot.command()
+# mod commands
+@bot.command(brief="Announce the current POTW to #potw")
 @commands.has_role('organizer')
-async def assign_point(ctx, user, pointvalue):
-    # add point value to database
-    func.assign_points(user, pointvalue)
-    await ctx.channel.send(f"{pointvalue} points has been assigned to <@{user}>")
+async def announce_potw(ctx):
+    channel = bot.get_channel(891482447580123137) #announcements
+    await ctx.channel.send(f"""Field: {current_potw['field']}
+    Problem ID: {current_potw['potw-id']}
+    Author: {current_potw['author']}
+    Difficulty: {current_potw['difficulty']} jalapeños
+    Problem statement: {current_potw['text']}""")
 
-@bot.command(brief="Print leaderboard")
+
+@bot.command(brief="Assign points (user ID, points to assign)")
 @commands.has_role('organizer')
+async def assign_point(ctx, user: discord.User, pointvalue):
+    # add point value to database
+    func.assign_points(user.id, pointvalue)
+    await ctx.channel.send(f"{pointvalue} points has been assigned to <@{user.id}>")
+
+    await user.send(f"You have just been awarded {pointvalue} points!")
+
+
+@bot.command(brief="Send a rejection message to the user")
+@commands.has_role('organizer')
+async def reject_solution(ctx, user: discord.User):
+    await user.send("Sorry, your solution to the current POTW has been rejected by the mods. Please try again, or contact Jewon or Ryan for more info.")
+
+
+@bot.command(brief="Print leaderboard") # top 10 people only + user
 async def leaderboard(ctx):
     with open('leaderboard.yaml', 'r') as f:
         leaderboard = yaml.load(f, Loader = yaml.FullLoader)
@@ -64,14 +90,20 @@ async def leaderboard(ctx):
     message = ""
 
     for i in range(0, len(sorted_leaderboard)):
-        message += f"{i+1} | <@{sorted_leaderboard[i]['userID']}> with {sorted_leaderboard[i]['points']} points \n"
-    
+        if i < 10:
+            message += f"{i+1} | <@{sorted_leaderboard[i]['userID']}> with {sorted_leaderboard[i]['points']} points\n"
+        elif ctx.message.author.id == sorted_leaderboard[i]['userID']:
+            message += f".\n.\n.\nYOU: {i+1} | <@{sorted_leaderboard[i]['userID']}> with {sorted_leaderboard[i]['points']} points"
+            break
+
     await ctx.channel.send(message)
 
-#TODO: let people know how many points they have
-#TODO: find way to incorporate username without pinging people
-#TODO: problem proposals (maybe not)
-#TODO: let people know if their score has been updated or that their solution was rejected (DMs)
+'''
+@bot.command()
+async def test(ctx, userID):
+    user = await bot.fetch_user(int(userID))
+    await ctx.channel.send(f"{user.display_name}\n{user.name}\n{user.discriminator}")
+'''
 
 # run the bot
 bot.run(TOKEN)
--- func.py
+++ func.py
@@ -5,7 +5,25 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
 
-def assign_points(user, points):
+def check_solved(user, potwID):
+    with open('leaderboard.yaml', 'r') as f:
+        leaderboard = yaml.load(f, Loader = yaml.FullLoader)
+
+    with open('potw.yaml', 'r') as f:
+        potw_contents = yaml.load(f, Loader = yaml.FullLoader)
+    current_potw = potw_contents['current-potw']
+
+    for person in leaderboard:
+        if str(person['userID']) == str(user):
+            for solved in person['solved-list']:
+                if solved == potwID:
+                    return True
+
+    return False
+
+def assign_points(userID, points):
+    user = bot.fetch_user(int(userID))
+
     with open('leaderboard.yaml', 'r') as f:
         leaderboard = yaml.load(f, Loader = yaml.FullLoader)
 
@@ -16,15 +34,19 @@ def assign_points(user, points):
     user_in_leaderboard = False
 
     for person in leaderboard:
-        if str(person['userID']) == str(user):
+        if str(person['userID']) == str(userID):
             person['points'] = int(person['points']) + int(points)
-            solved_list = person['solved-list']
-            solved_list.append(current_potw['potw-id'])
-            person['solved-list'] = solved_list
+
+            if check_solved(userID, current_potw['potw-id']) == False:
+                solved_list = person['solved-list']
+                solved_list.append(current_potw['potw-id'])
+                person['solved-list'] = solved_list
+            
             user_in_leaderboard = True
 
     if user_in_leaderboard == False:
-        person = {'points': int(points), 'solved-list': [current_potw['potw-id']], 'userID': int(user), 'username': bot.get_user(int(user))}
+
+        person = {'points': int(points), 'solved-list': [current_potw['potw-id']], 'userID': userID, 'username': f"{user.name}#{user.discriminator}"}
         leaderboard.append(person)
 
     with open('leaderboard.yaml', 'w') as f:
